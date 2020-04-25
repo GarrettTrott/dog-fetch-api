53878e9a34135a1e088b49a8b3a69e946bc73d44
Garrett Trott <garretttrott@gmail.com>
2020-04-24 23:55:32 -0700

    adds fetchData function

--- js/app.js
+++ js/app.js
@@ -6,13 +6,28 @@ const form = document.querySelector("form");
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((response) => response.json())
-  .then((data) => generateImage(data.message));
+function fetchData(url) {
+  return fetch(url).then((res) => res.json());
+}
+
+fetchData("https://dog.ceo/api/breeds/list").then((data) =>
+  generateOptions(data.message)
+);
+
+fetchData("https://dog.ceo/api/breeds/image/random").then((data) =>
+  generateImage(data.message)
+);
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
+function generateOptions(data) {
+  const options = data
+    .map((item) => `<option value='${item}'>${item}</option>`)
+    .join("");
+  select.innerHTML = options;
+}
 
 function generateImage(data) {
   const html = `
