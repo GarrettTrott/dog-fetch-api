bc238a49ea3b44bfbeaad94ecda48f2e09084927
Garrett Trott <garretttrott@gmail.com>
2020-04-25 00:27:43 -0700

    adds promise all

--- js/app.js
+++ js/app.js
@@ -7,20 +7,34 @@ const form = document.querySelector("form");
 // ------------------------------------------
 
 function fetchData(url) {
-  return fetch(url).then((res) => res.json());
+  return fetch(url)
+    .then(checkStatus)
+    .then((res) => res.json())
+    .catch((error) => console.log("Looks like there was a problem", error));
 }
 
-fetchData("https://dog.ceo/api/breeds/list").then((data) =>
-  generateOptions(data.message)
-);
+Promise.all([
+  fetchData("https://dog.ceo/api/breeds/list"),
+  fetchData("https://dog.ceo/api/breeds/image/random"),
+]).then((data) => {
+  const breedList = data[0].message;
+  const randomImage = data[1].message;
 
-fetchData("https://dog.ceo/api/breeds/image/random").then((data) =>
-  generateImage(data.message)
-);
+  generateOptions(breedList);
+  generateImage(randomImage);
+});
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
 
 function generateOptions(data) {
   const options = data
@@ -37,9 +51,23 @@ function generateImage(data) {
   card.innerHTML = html;
 }
 
+function fetchBredImage() {
+  const breed = select.value;
+  const img = card.querySelector("img");
+  const p = card.querySelector("p");
+
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`).then((data) => {
+    img.src = data.message;
+    img.alt = breed;
+    p.textContent = `Click to view more ${breed}s`;
+  });
+}
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
+
+select.addEventListener("change", fetchBredImage);
+card.addEventListener("click", fetchBredImage);
 
 // ------------------------------------------
 //  POST DATA
